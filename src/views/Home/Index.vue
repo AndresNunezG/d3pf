8caaca7311c86dfc43c5c5fb012df62905b806b9
Andres Nunez <camilonunezz9812@gmail.com>
2022-01-22 15:52:05 -0500

    home title

--- src/views/Home/Index.vue
+++ src/views/Home/Index.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="home">
-    <h1>Home Page</h1>
+    <HomeTitle/>
   </div>
 </template>
 
 <script>
+import HomeTitle from './HomeTitle.vue'
 
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    HomeTitle
+  }
 }
 </script>
