cf12faadf0efcae1918982c6e61cb33adb18b672
Andres Nunez <camilonunezz9812@gmail.com>
2022-02-05 22:33:12 -0500

    components in table slots (HeroClass and HeroIco)

--- src/views/Profile/MainBlock/HeroesList/Index.vue
+++ src/views/Profile/MainBlock/HeroesList/Index.vue
@@ -1,13 +1,38 @@
 <template>
     <div>
         <h1>Heroes List</h1>
-        <b-table dark hover small striped stacked="sm" :items="heroes" :fields="fields"/>
+        <b-table
+          dark
+          hover
+          small
+          striped
+          stacked="sm"
+          :items="heroes"
+          :fields="fields"
+        >
+          <template v-slot:cell(name)="data">
+            <HeroIco :hero="data.item"/>
+          </template>
+          <template v-slot:cell(class)="data">
+            <HeroClassLevel :hero="data.item"/>
+          </template>
+          <template v-slot:cell(kills)="data">
+            <span>{{ data.item.kills.elites }}</span>
+          </template>
+        </b-table>
     </div>
 </template>
 
 <script>
+import HeroIco from './HeroIco.vue'
+import HeroClassLevel from './HeroClassLevel.vue'
+
 export default {
   name: 'HeroesList',
+  components: {
+    HeroIco,
+    HeroClassLevel
+  },
   props: {
     heroes: {
       required: true,
